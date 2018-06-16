d93caff3e448878fab3ed533e139c720265e6179
joshtecx <32961912+joshtecx@users.noreply.github.com>
2018-06-16 09:36:11 -0400

    deploying to heroku

--- public/app.js
+++ public/app.js
@@ -1,51 +1,94 @@
-// Get articles in json form
-$.getJSON("/articles", function(data){
-    for(let i = 0; i < data.length; i++){
-        $("#articles").append(`<p data-id=${data[i]._id} <br /> ${data[i].link} </p>`);
-    }
-});
-
-$(document).on("click", "p", function(){
-    $("#notes").empty();
-    const thisId = $(this).attr("data-id");
-
+$(document).on("click", "#scraper", function(){
+    console.log("click");
     $.ajax({
         method: "GET",
-        url: "/articles/" + thisId
-    })
+        url: "/scrape"
+    }).then(function(data){
+        $.ajax({
+            method: "GET",
+            url: "/"
+        })
         .then(function(data){
-            console.log(data);
-            $("#notes").append(`<h2> ${data.title} </h2>`);
+            location.reload();
+        })
 
-            $("#notes").append(`<input id="titleinput" name="title" >`);
-
-            $("#notes").append(`<textarea id="bodyinput" name="body"></textarea>`);
-
-            $("#notes").append(`<button data-id=${data._id} id="savenote">Save Note</button>`);
-
-            if(data.note){
-                $("#titleinput").val(data.note.title);
-                $("#bodyinput").val(data.note.body);
-            }
-
-        });
+    });
 });
 
-$(document).on("click", "#savenote", function(){
+$(document).on("click", ".submit", function(event){
+    event.preventDefault();
+    console.log("click");
     const thisId = $(this).attr("data-id");
-
     $.ajax({
         method: "POST",
-        url: "/articles/"+ thisId,
+        url: "/articles/" + thisId,
         data: {
-            title: $("#titleinput").val(),
-            body: $("#titleinput").val()
+            body: $(`#Note${id}`).val()
         }
-    })
-        .then(function(data){
-            console.log(data);
-            $("#notes").empty();
-        });
-        $("#titleinput").val("");
-        $("#titleinput").val("");
+    });
+    $(".noteArea").val("");
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Get articles in json form
+// $.getJSON("/articles", function(data){
+//     for(let i = 0; i < data.length; i++){
+//         $("#articles").append(`<p data-id=${data[i]._id} <br /> ${data[i].link} </p>`);
+//     }
+// });
+
+// $(document).on("click", "p", function(){
+//     $("#notes").empty();
+//     const thisId = $(this).attr("data-id");
+
+//     $.ajax({
+//         method: "GET",
+//         url: "/articles/" + thisId
+//     })
+//         .then(function(data){
+//             console.log(data);
+//             $("#notes").append(`<h2> ${data.title} </h2>`);
+
+//             $("#notes").append(`<input id="titleinput" name="title" >`);
+
+//             $("#notes").append(`<textarea id="bodyinput" name="body"></textarea>`);
+
+//             $("#notes").append(`<button data-id=${data._id} id="savenote">Save Note</button>`);
+
+//             if(data.note){
+//                 $("#titleinput").val(data.note.title);
+//                 $("#bodyinput").val(data.note.body);
+//             }
+
+//         });
+// });
+
+// $(document).on("click", "#savenote", function(){
+//     const thisId = $(this).attr("data-id");
+
+//     $.ajax({
+//         method: "POST",
+//         url: "/articles/"+ thisId,
+//         data: {
+//             title: $("#titleinput").val(),
+//             body: $("#titleinput").val()
+//         }
+//     })
+//         .then(function(data){
+//             console.log(data);
+//             $("#notes").empty();
+//         });
+//         $("#titleinput").val("");
+//         $("#titleinput").val("");
+// });
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const db = require("./models");
 const axios = require("axios");
 const logger = require("morgan");
 const expHbars = require('express-handlebars');
+const path = require("path");
 
 // INITIALIZE EXPRESS
 const app = express();
@@ -19,7 +20,7 @@ const PORT = 3000;
 // MIDDLEWARE
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 // HANDLEBARS
 app.set('views', path.join(__dirname, 'views'));
@@ -31,14 +32,14 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoNews";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
-
+const result = {};
 // ROUTES
 app.get("/scrape", function(req, res){
     axios.get("http://nytimes.com").then(function(response) {
         const $ = cheerio.load(response.data);
 
         $(".story-heading").each(function(i, element){
-            const result = {};
+            
 
             result.title = $(this)
                 .children("a")
@@ -49,6 +50,7 @@ app.get("/scrape", function(req, res){
             db.Article.create(result)
                 .then(function(dbArticle){
                     console.log(dbArticle)
+                    res.redirect("/");
                 })
                 .catch(function(err){
                     return res.json(err);
@@ -56,6 +58,15 @@ app.get("/scrape", function(req, res){
         });
         res.send("Scraped!");
     });
+});
+
+app.get("/", function(req, res){
+    db.Article.find({})
+        .then(function(dbArticle){
+           res.render("index", {result: dbArticle});
+        }).catch(function(err){
+            return res.json(err);
+        });
 });
 
 app.get("/articles", function(req, res) {
